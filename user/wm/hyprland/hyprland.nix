ab290c86ad7fefaa8f72a9bd30793dca73854474
= <=>
2025-04-07 23:04:57 -0500

    fixed hyprland

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -1,16 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, inputs, userSettings, ... }:
 
-{
+let
+  hyprlandPkg = inputs.hyprland.packages.${pkgs.system}.default;
+in {
   wayland.windowManager.hyprland = {
     enable = true;
-    package = pkgs.hyprland;
+    package = hyprlandPkg;
 
     settings = {
-      exec-once = [ "waybar &" "hyprpaper &" ];
+      exec-once = [
+        "waybar &"
+        "hyprpaper &"
+      ];
+
       input = {
         kb_layout = "us";
         follow_mouse = 1;
       };
+
       general = {
         gaps_in = 5;
         gaps_out = 20;
@@ -20,5 +27,6 @@
     };
   };
 
+  # Optional: portable config
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
 }
