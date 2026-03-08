962b8319be47b8780334a4e2170c0154d0b3b48e
Rob <67354942+DispatchCode3@users.noreply.github.com>
2026-03-08 12:06:25 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,71 +1,30 @@
 {
-  description = "Flake of DispatchCode3";
-
-  outputs = inputs@{ self, ... }:
-    let
-      # ---- SYSTEM SETTINGS ---- #
-      systemSettings = {
-        system = "x86_64-linux";
-        hostname = "rb3";
-        profile = "minimal"; # select a profile defined from my profiles directory
-        timezone = "America/Chicago";
-        locale = "en_US.UTF-8";
-        bootMode = "uefi"; # uefi or bios
-        bootMountPath = "/boot"; # only used for uefi boot mode
-        grubDevice = "/dev/sda"; # only used for bios boot mode
-        gpuType = "intel"; # amd, intel or nvidia
-      };
-      # ---- USER SETTINGS ---- #
-      userSettings = rec {
-        username = "rob";
-        name = "Rob";
-        email = "dispatchcode3@gmail.com";
-        dotfilesDir = "~/.dotfiles";   
-        browser = "firefox";
-        editor = "neovim";
-        term = "alacritty";
-        theme = "";
-        wm = "hyprland";
-        wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
-      };
-
-        pkgs = import inputs.nixpkgs {
-          system = systemSettings.system;
-          config.allowUnfree = true;
-        };
-        
-        lib = inputs.nixpkgs.lib;
-
-    in
-    {
-      nixosConfigurations.system = lib.nixosSystem {
-        system = systemSettings.system;
-        modules = [ 
-          (./. + "/profiles/${systemSettings.profile}/configuration.nix")
-        ];
-        specialArgs = {
-          inherit inputs systemSettings userSettings;
-        };
-      };
-
-      homeConfigurations.user = inputs.home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ 
-          (./. + "/profiles/${systemSettings.profile}/home.nix")
-          ];
-        extraSpecialArgs = {
-          inherit inputs systemSettings userSettings;
-        };
-      };
-    };
+  description = "Rob's bare-bones NixOS 25.11 system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+
+        home-manager.nixosModules.default
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rob = import ./home.nix;
+        }
+      ];
+    };
   };
 }
