19a4f79ad17b377a7e7455de642196ece49048e1
Rob <67354942+DispatchCode3@users.noreply.github.com>
2026-03-08 12:06:48 -0500

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "nixos";
+
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  users.users.rob = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "changeme";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.windowManager.qtile.enable = true;
+
+  services.displayManager.ly.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  programs.alacritty.enable = true;
+
+  system.stateVersion = "25.11";
+}
