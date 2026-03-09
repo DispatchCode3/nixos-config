50aa1b4440447e596431249b20a12e7a2422a23d
Rob <67354942+DispatchCode3@users.noreply.github.com>
2026-03-08 21:06:20 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,41 @@
+let
+  settings = import ./settings.nix;
+in
 {
-  description = "Rob's bare-bones NixOS 25.11 system";
+  description = "Minimal NixOS system with base Home Manager defaults and user overrides";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-${settings.nixosRelease}";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-${settings.nixosRelease}";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { nixpkgs, home-manager, ... }:
     {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      nixosConfigurations.${settings.hostName} = nixpkgs.lib.nixosSystem {
+        system = settings.system;
+
+        specialArgs = {
+          inherit settings;
+        };
+
         modules = [
-          ./hardware-configuration.nix
-          ./configuration.nix
+          ./hosts/nixos/default.nix
+          ./hosts/nixos/hardware-configuration.nix
+
           home-manager.nixosModules.default
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.rob = import ./home.nix;
+            home-manager.extraSpecialArgs = {
+              inherit settings;
+            };
+
+            home-manager.users.${settings.userName} =
+              import (./users + "/${settings.userName}.nix");
           }
         ];
       };
